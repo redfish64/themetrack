771c13492ede1c7d48f2cb3b2cda72feac32f76b
Tim Engler <engler@gmail.com>
2024-06-29 16:05:19 +0800

    fixed problem with welcome msg

--- scripts/pyinstaller.bat
+++ scripts/pyinstaller.bat
@@ -3,6 +3,7 @@ cd ..
 pyinstaller.exe --name themetrack --icon=images\themetrack.ico themetrack.py ^
 --add-data system_rules.xlsx:. ^
 --add-data theme_track_config.xlsx:. ^
---add-data dos_scripts/dos_greeting.txt:dos_scripts ^
 --add-data dos_scripts/welcome.bat:dos_scripts
 
+mkdir dist\themetrack\dos_scripts
+copy dos_scripts\dos_greeting.txt dist\themetrack\dos_scripts\
